5f0febc27ce818aa8648e9317964ba198150d861
Alok Singh <a1singh@ucsd.edu>
2016-05-20 16:53:43 -0700

    basic structure

--- simulation.py
+++ simulation.py
@@ -1,11 +1,12 @@
 #! python
 
-
 #initialize global state
 
-#Fill user hashmap: ['userid'] -> ['nickname': '',  'twitter': '',  'dob': '',  'country': '',  'timeStamp': '']
+#Fill user hashmap: 
+		#['userid'] -> ['nickname': '',  'twitter': '',  'dob': '',  'country': '',  'timeStamp': '', 'tags'=[gameaccuracy, purchbeh, adbeh, chatbeh] ]
+		# globalUsers = createUserDatabase(minAge, maxAge, meanAge) //returns a hash map
 
-#Fill team hashmap: ['teamid'] -> ['name': '',  'teamCreationTime': '',  'teamEndTime': '']
+#Fill team hashmap: ['teamid'] -> ['name': '',  'teamCreationTime': '',  'teamEndTime': '', 'strength': '0-1']
 
 #Fill user-team assignment current-state hashmap ['assignmentid']->['userid': '', teamid': '',  'sessionid': '',]
 
@@ -13,25 +14,39 @@
 
 #Fill team current-state hashmap: ['teamid']->['level': '', 'members': [] ]
 
-
+#dayLength: Int (mins)
 
 
 # for every team T 
 		# decide team levels at the end of this day
+			#endOfDayLevel = howManyLevelsToday(l: thisLevel, T:Team)
+					#last level is unfinished
+					#end = f(no of sessions in T, x, strength)
 
-		# for each level L crossed today: 
+		# for each level L crossed today: 'time=TD'
 
 			# C = number of clicks needed at this level
 			# pick 'x' users from T who will generate these hits
 			# fill the 'GameClicks' file with rows for each hit
+				# users = getGameClicksUsers(T: Team, N: Int, TD: Time) //return a list of users from playing ones
 
-			# pick 'y' users from T who will click on Ads
-			# fill the AdClicks file with rows for each click
+			# fill the AdClicks file with rows for each click - based on user tags
+				# users = getAdClickUsers(T: Team, TD: Time)  //return a list of users from playing ones
 
-			# pick 'z' users from T who will make purchases
-			# fil the InAppPurchases file with rows for each click
+			# fill the InAppPurchases file with rows for each click - based on user tags
+				# users = getPurchasesUsers(T: Team, TD: Time) //return a list of pairs (user, price)
+
+			# fill the Chat table with users from team T - based on user tags
+				#fillChatTable(T, maxMentions: Int, TD: Time)
 
 			# update global state for next day
-				# create new teams
-				# end old teams
+				# call f(p: Int, TD: Time) = updates user team assignments for next day ()
+								# create new teams
+								# end old teams
+								# change p% of user assignments
 				# change user-team assignment
+				# update sessions
+
+			#TD=TD+dayDuration
+
+
