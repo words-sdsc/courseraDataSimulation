b15a8163f957bb61209d9b429cab77f449097f60
Charles H. Chen <chencharlesh@gmail.com>
2016-05-22 00:37:34 -0700

    Added proper user_id for game_clicks.py

--- game_clicks.py
+++ game_clicks.py
@@ -4,17 +4,15 @@ import random
 globalUsers = global_vars.globalUsers
 
 def getGameClickUsers(team, numUsers, time):
-	#Grab users to fill
-	randUsers = random.sample(globalUsers, numUsers)
+	# Grab users to fill.
+	randIDS= random.sample(globalUsers.viewKeys(), numUsers)
 	gameClickFileBuf = []
 
 	# Not sure how to generate this.
 	userSession = 0
 
-	userId = 0
-	for users in randUsers:
-		gameClickFileBuf.append({userId, userSession, time, random.uniform(0,1)})
-		userId += 1
+	for userID in randIDS:
+		gameClickFileBuf.append({userID, userSession, time, random.uniform(0,1)})
 
 	# Data created, flush it to file.
 	# TODO: Add file flush.
