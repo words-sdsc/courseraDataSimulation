7a43192e6c90de2fec1ee8b2b253156446c95c5a
Alok Singh <a1singh@ucsd.edu>
2016-05-23 12:14:23 -0700

    sessions working

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ def main():
 	userDatabaseList = createUserDatabase(2000) #userID = index on the list
 	teamDatabaseList = createTeamDatabase(100)  #teamID = index on the list
 	assignmentsList = asssignUsersTOteams(userDatabaseList, teamDatabaseList)
-	userSessionsList = initializeUserSessions(assignmentsList)
+	userSessionsList = initializeUserSessions(assignmentsList, teamDatabaseList)
+
+	#helper functions:
+	playingMembers 	= getPlayingMembers(userSessionsList, assignmentsList) # ['teamid']->[user1, user2,...] (have open sessions)
+	allMembers  	= getAllMembers(assignmentsList) #['teamid']->[user1, user2,...] (all members)
 
 if __name__ == "__main__":
 	main()
