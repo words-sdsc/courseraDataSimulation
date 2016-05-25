d70f8c49dff1c1c16514c976448d218977555463
Charles H. Chen <chencharlesh@gmail.com>
2016-05-25 16:53:18 -0700

    Added more framework for update_day.py

--- update_day.py
+++ update_day.py
@@ -1,1 +1,54 @@
 import global_vars
+
+# Team assignment buffer used for housing all team writes.
+teamAssignBuffer = []
+
+# Contains Level Events format for flushing.
+levelUpBuffer = []
+
+# Contains data to write for ENDING teams.
+teamBuffer = []
+
+# Contains user session info to write to file.
+userSessionBuffer = []
+
+def simulateNextDay():
+	levelUp()
+	userMovement()
+
+	# Flush the buffers for writing.
+	flushWriteTeams()
+	flushTeamAssign()
+	flushLevelUp()
+	flushUserSession()
+
+
+# Moves users left and right.
+def userMovement():
+	checkTeamEmpty()
+
+# Function to check if given team is empty, if so, write to buffer.
+def checkTeamEmpty(team):
+
+# Function to check if a team has leveled up from previous day.
+def levelUp():
+
+# Write the teams buffer.
+def flushWriteTeams():
+
+	teamBuffer = []
+
+# Write the assign teams buffer.
+def flushTeamAssign():
+
+	teamAssignBuffer = []
+
+# Write the level up buffer.
+def flushLevelUp():
+
+	levelUpBuffer = []
+
+# Writes the user session buffer.
+def flushUserSession():
+
+	userSessionBuffer = []
