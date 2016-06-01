ff8d2f747372e9261f5c5bcbaf3de0a0f4037913
crawl <danielcrawl@gmail.com>
2016-06-01 16:37:49 -0700

    finishing test

--- calc/test_teamLevelAdvance.py
+++ calc/test_teamLevelAdvance.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-# TEST 2 
-# TEST 5
+# TEST 1
 
+import math
 import warnings
 warnings.filterwarnings("ignore", category=FutureWarning) 
 
@@ -15,17 +15,42 @@ def value(item):
 
 #*********************************** test 1
 
-df = pd.read_table('../level-events.log', header=None, delimiter=',',
-                   converters={i:value for i in range(5)},
-                   names='time eventid teamid level eventType'.split())
+#teamdf = pd.read_table('../team.log', header=None, delimiter=',',
+	#converters={i:value for i in range(6)},
+	#names='teamid name teamCreationTime teamEndTime strength currentLevel'.split())
+#teamdf = teamdf.convert_objects(convert_numeric=True)
 
+passalltests = True
 
-df = df.convert_objects(convert_numeric=True)
+levdf = pd.read_table('../level-events.log', header=None, delimiter=',',
+	converters={i:value for i in range(5)},
+	names='time eventid teamid level eventType'.split())
+levdf = levdf.convert_objects(convert_numeric=True)
+
+sesdf = pd.read_table('../user-session.log', header=None, delimiter=',',
+	converters={i:value for i in range(8)},
+	names='userSessionid userid teamid assignmentid startTimeStamp endTimeStamp team_level platformType'.split())
+sesdf = sesdf.convert_objects(convert_numeric=True)
+
+print '\nNo of level increases found in level-events.log = ', len(levdf[levdf['eventType'] == 'end'])
+
+for teamid in sorted(levdf['teamid'].unique()):
+	#print 'Checking team', teamid
+	maxLevel = levdf[levdf['teamid'] == teamid]['level'].max()
+	if not math.isnan(maxLevel):
+		#print '  max level is', maxLevel
+
+		for level in range(2,maxLevel):
+			#print 'check', level
+
+			# check max session time for at level-1 is less than min start time at level
+			endTimePrevLevel = sesdf[(sesdf['teamid'] == teamid) & (sesdf['team_level'] == (level-1))]['endTimeStamp'].max()
+			startTimeLevel = sesdf[(sesdf['teamid'] == teamid) & (sesdf['team_level'] == level)]['startTimeStamp'].min()
+			if endTimePrevLevel > startTimeLevel:
+				print 'ERROR, team', teamid, 'session timestamps wrong for level', level, 'max end=', endTimePrevLevel, 'min start=', startTimeLevel
+				passalltests = False
 
 #print df
 
-#df = df.groupby(by=['teamId','teamLevel'],as_index=False)['isHit'].sum()
-
-print '\nNo of level increases found in level-events.log = ', len(df[df['eventType'] == 'end'])
-
-passalltests = True
+if passalltests:
+	print('[TEST 1 PASS] Sessions closed for team members after level advancement.\n')
