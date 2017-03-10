3d75324b6f4d87f2f33cbc8082443fb7ed0b57cf
Alok Singh <a1singh@ucsd.edu>
2017-03-09 22:15:01 -0800

    e

--- capstone/readings/setupWeek3.sh
+++ capstone/readings/setupWeek3.sh
@@ -1,16 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
 #Run this script ONCE on Cloudera VM
-#This script prepares Cloudera VM for sparkMLlibCustering.py
+#This script prepares Cloudera VM 
 
-#update Spark
-sudo yum install -y spark-core spark-master spark-worker spark-history-server spark-python
+# upgrade spark python to work with python 3
+sudo yum upgrade -y spark-python hive
 
-#get Anaconda  
+# remove old derby jar so only one version is on spark's classpath
+sudo rm /usr/lib/flume-ng/lib/derby-10.8.2.2.jar
+
+# download and install anaconda for pandas, jupyter
+rm -f Anaconda3-4.0.0-Linux-x86_64.sh
 wget http://repo.continuum.io/archive/Anaconda3-4.0.0-Linux-x86_64.sh
-
-#install Anaconda
 bash Anaconda3-4.0.0-Linux-x86_64.sh
+
+# add spark-csv jars to classpath
+echo export SPARK_CLASSPATH="$(pwd)/lib/spark-csv_2.10-1.5.0.jar:$(pwd)/lib/commons-csv-1.1.jar" >> ~/.bashrc
+
+# set environment variables to load spark libs in jupyter
+echo "export PYSPARK_DRIVER_PYTHON_OPTS=\"notebook\"" >> ~/.bashrc
+echo "export PYSPARK_DRIVER_PYTHON=jupyter"  >> ~/.bashrc
+
+echo "Run 'source ~/.bashrc' to complete the setup."
 
 # _________when you see --More-- keep pressing SPACE BAR
 # _________answer yes to all questions
+
